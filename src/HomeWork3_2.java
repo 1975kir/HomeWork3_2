58b436914b2aa793299dff1cc48fd0c489a5614a
1975km <km1975@yandex.ru>
2022-02-21 19:03:24 +0300

    Initial commit

---
+++ src/HomeWork3_2.java
@@ -0,0 +1,48 @@
+import Worker.Worker;
+import java.util.Scanner;
+
+public class HomeWork3_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Worker Wkr1 = new Worker();
+        Wkr1.fullname = "Иван Сергеевич Иванов";
+        Wkr1.post = "Инженер";
+        Wkr1.email = "Ivan@mail.ru";
+        Wkr1.phone = "666555";
+        Wkr1.salary = "50000";
+        Wkr1.age = "60";
+
+        Worker Wkr2 = new Worker();
+        Wkr1.fullname = "Дмитрий Захарович Сидоров";
+        Wkr1.post = "Бухгалтер";
+        Wkr1.email = "Dima@mail.ru";
+        Wkr1.phone = "777888";
+        Wkr1.salary = "55000";
+        Wkr1.age = "50";
+
+        Worker Wkr3 = new Worker();
+        Wkr1.fullname = "Виталий Иванович Петров";
+        Wkr1.post = "Сварщик";
+        Wkr1.email = "Vital@mail.ru";
+        Wkr1.phone = "888999";
+        Wkr1.salary = "60000";
+        Wkr1.age = "30";
+
+        Worker[] Workers = new Worker[3];
+        Workers[0] = Wkr1;
+        Workers[1] = Wkr2;
+        Workers[2] = Wkr3;
+
+        for (int i = 0; i < Workers.length; i++) {
+            if (Workers[i].age > 40) {
+                Workers[i].WkrInfo();
+            }
+
+        }
+
+
+        }
+
+
+    }
+
---
+++ src/Worker/Worker.java
@@ -0,0 +1,10 @@
+package Worker;
+
+public class Worker {
+    public String fullname;
+    public String post;
+    public String email;
+    public String phone;
+    public String salary;
+    public int age;
+}
